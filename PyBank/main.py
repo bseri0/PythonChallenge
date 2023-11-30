e5a0a3e00894becb5e2166266c4cf909f569781c
Bryan S <Bryan.serino0513@gmail.com>
2023-11-29 21:57:52 -0500

    modified:   PyBank/analysis/budget_data_res.csv 	modified:   PyBank/main.py 	new file:   PyBank/main_old.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,41 +1,52 @@
 import os
 import csv
-import pandas as pd
-import contextlib
 
 print("CUR DIR:  " + str(os.getcwd()))
+
 # Relative path to the input CSV file
-path1 = os.path.join(os.getcwd(),'module_3')
+path1 = os.path.join(os.getcwd(), 'module_3')
 print("path1:" + path1)
-input_csv_file = os.path.join(path1,'PyBank','resources', 'budget_data.csv')
+input_csv_file = os.path.join(path1, 'PyBank', 'resources', 'budget_data.csv')
 print("Input CSV path:" + input_csv_file)
+
+# Read the CSV file and store the data in a list of dictionaries
+with open(input_csv_file, 'r') as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    data = list(csvreader)
+
+# Calculate the number of months
+months = len(data)
+
+# Calculate the total profit/losses
+summ = sum(int(row['Profit/Losses']) for row in data)
+
+# Calculate the average profit/losses per month
+avg = summ / months
+
+# Find the minimum and maximum values in the `Profit/Losses` column
+min_row = min(data, key=lambda x: int(x['Profit/Losses']))
+max_row = max(data, key=lambda x: int(x['Profit/Losses']))
+
+# Print the results
+print(f"Months: {months}")
+print(f"Sum: {summ}")
+print(f"The Average: {avg}")
+print(f"The minimum value: {min_row}")
+print(f"The maximum value: {max_row}")
+
 # Relative path to the output CSV file
-output_csv_file = os.path.join(path1,'PyBank','analysis', 'budget_data_res.csv')
+output_csv_file = os.path.join(path1, 'PyBank', 'analysis', 'budget_data_res.csv')
 
-with contextlib.redirect_stdout(open(output_csv_file, 'w')):
+# Write the results to the output CSV file
+with open(output_csv_file, 'w', newline='') as csvfile:
+    fieldnames = ['Months', 'Sum', 'Average', 'Minimum Value', 'Maximum Value']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    # Read the CSV file data set into a pandas DataFrame
-    df = pd.read_csv(input_csv_file)
-
-    # Print the DataFrame
-    print(df.to_string())
-
-    # Calculate the number of months
-    months = len(df.groupby('Date'))
-
-    # Calculate the total profit/losses
-    summ = sum(df['Profit/Losses'])
-
-    # Calculate the average profit/losses per month
-    avg = summ / months
-
-    # Calculate the minimum and maximum values in the `Profit/Losses` column
-    min_row = df.loc[df['Profit/Losses'].idxmin()]
-    max_row = df.loc[df[['Profit/Losses']].idxmax()]
-
-    # Print the results
-    print(f"Months: {months}")
-    print(f"Sum: {summ}")
-    print(f"The Average : {avg}")
-    print(f"The minimum value: {min_row}")
-    print(f"The maximum value: {max_row}")
+    writer.writeheader()
+    writer.writerow({
+        'Months': months,
+        'Sum': summ,
+        'Average': avg,
+        'Minimum Value': min_row['Profit/Losses'],
+        'Maximum Value': max_row['Profit/Losses']
+    })
